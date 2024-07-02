37bb2443bda44484511976d2768d8fac157c22f3
alsguert <alsguert@gmail.com>
2024-07-02 08:13:12 -0300

    fix aula_js01

--- aula_js01/main.js
+++ aula_js01/main.js
@@ -1,4 +1,16 @@
-var peso = prompt("Qual é seu o peso?(kg)")
-var altura = prompt("Qual é sua altura?(metros)")
+var peso = parseFloat(prompt("Qual é o seu peso em kg?"))
+var altura = parseFloat(prompt("Qual é a sua altura?"))
 
-var imc = peso / (altura ^ 2)
+var imc = peso / (altura * altura)
+
+alert("Seu IMC é: " + imc.toFixed(2))
+
+if (imc < 18.5) {
+    alert("O IMC indica baixo peso.");
+} else if (imc >= 18.5 && imc <= 24.9) {
+    alert("O IMC indica peso normal.");
+} else if (imc >= 25 && imc <= 29.9) {
+    alert("O IMC indica sobrepeso.");
+} else {
+    alert("O IMC indica obesidade.");
+}
